19cb90c074ae0edb5ad737374488faba02e85d31
18521367 <79902336+DuongTam1410@users.noreply.github.com>
2021-03-15 03:40:59 +0700

    Update BRG2Gray.py

--- Assignments/Week2/BRG2Gray.py
+++ Assignments/Week2/BRG2Gray.py
@@ -1,13 +1,19 @@
 import cv2 as cv
 import numpy as np
 
+#Read and show image
 img = cv.imread('Velkoz.jpg')
 cv.imshow("Ảnh màu",img)
 cv.waitKey()
 
+#Convert BGR to Gray
 img_gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+
+#Resize image
 img_gray_rs = cv.resize(img_gray,dsize=(500,400))
 cv.imwrite("velkoz_gray.jpg",img_gray_rs)
+
+#Save image
 cv.imshow("Ảnh sau khi format",img_gray_rs)
 cv.waitKey()
 cv.destroyAllWindows()
