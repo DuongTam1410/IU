365e705a0d4633ce090443a05757f63321a28993
18521367 <18521367@gm.uit.edu.vn>
2021-03-15 20:30:19 +0700

    Add Convert Img2Binary

--- Assignments/Week2/Python/BRG2Gray.py
+++ Assignments/Week2/Python/BRG2Gray.py
@@ -10,7 +10,7 @@ cv.waitKey()
 img_gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 
 #Resize image
-img_gray_rs = cv.resize(img_gray,dsize=(500,400))
+img_gray_rs = cv.resize(img_gray,dsize=(500,500))
 cv.imwrite("velkoz_gray.jpg",img_gray_rs)
 
 #Save image
