435045143ad2e8798c65f9ec55b6384dfd2c7b56
18521367 <18521367@gm.uit.edu.vn>
2021-03-21 14:07:18 +0700

    Compare2Img

---
+++ Assignments/Week2/Python/Float2Img.py
@@ -0,0 +1,52 @@
+import numpy as np
+import cv2 as cv
+import struct
+
+def rgb2gray(r,g,b):
+    gray = (0.2989*r + 0.5870*g + 0.1140*b)
+    return gray
+
+def bin_to_float(binary):
+    return struct.unpack('!f',struct.pack('!I', int(binary, 2)))[0]
+
+def float_to_bin(num):
+    return format(struct.unpack('!I', struct.pack('!f', num))[0], '032b')
+
+image = np.zeros((500, 500, 3), np.uint8)
+
+f1 = open('iredfloat.txt', 'r')
+f2 = open('igreenfloat.txt', 'r')
+f3 = open('ibluefloat.txt', 'r')
+f4 = open('Python.txt','w')
+
+pixelsb = []
+pixelsg = []
+pixelsr = []
+gray = []
+
+for x1 in f3:
+    pixelsb.append(int(bin_to_float(x1)*255))
+for x2 in f2:
+    pixelsg.append(int(bin_to_float(x2)*255))
+for x3 in f1:
+    pixelsr.append((bin_to_float(x3)*255))
+
+i = 0
+j = 0
+k = 0
+
+for i in range(500):
+    for j in range(500):
+        f4.write(float_to_bin(rgb2gray((pixelsr[k]), (pixelsg[k]), (pixelsb[k]))) + '\n')
+        image[i, j] = rgb2gray((pixelsr[k]), (pixelsg[k]), (pixelsb[k]))
+        k = k + 1
+
+# Write image
+cv.imwrite('GrayPython.jpg', image)
+cv.imshow('GrayPython',image)
+cv.waitKey()
+cv.destroyAllWindows()
+f1.close()
+f2.close()
+f3.close()
+f4.close()
---
+++ Assignments/Week2/Python/Sosanh.py
@@ -0,0 +1,49 @@
+import struct
+import numpy as np
+import cv2 as cv
+
+def bin_to_float(binary):
+    return struct.unpack('!f', struct.pack('!I', int(binary, 2)))[0]
+pp = 500
+image1 = np.zeros((pp, pp, 3), np.uint8)
+image2 = np.zeros((pp, pp, 3), np.uint8)
+
+f1 = open('verilog.txt', 'r')
+f2 = open('Python.txt','r')
+Compare = open('Compare.txt','w')
+
+pixelsverilog = []
+pixelspython = []
+count = 0
+sum = 0
+max = 0
+min =  float('inf')
+
+for x1 in f1:
+    pixelsverilog.append(bin_to_float(x1))
+for x2 in f2:
+    pixelspython.append(bin_to_float(x2))
+
+i = 0
+j = 0
+k = 0
+
+for i in range(pp):
+    for j in range(pp):
+        image1[i, j] = (pixelsverilog[k], pixelsverilog[k], pixelsverilog[k])
+        image2[i, j] = (pixelspython[k], pixelspython[k], pixelspython[k])
+        Compare.write(str(abs(pixelsverilog[k]-pixelspython[k])) + '\n')
+        if ((abs(pixelsverilog[k]-pixelspython[k]) > max )):
+            max = abs(pixelsverilog[k]-pixelspython[k])
+        k = k + 1
+
+# Write image
+print('Sai so lon nhat la:',max)
+cv.imwrite('Verilog.jpg', image1)
+cv.imshow('Verilog',image1)
+cv.imwrite('Python.jpg', image2)
+cv.imshow('Python',image2)
+cv.waitKey()
+cv.destroyAllWindows()
+f1.close()
+f2.close()
